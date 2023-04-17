302856a458f044901b20d7af295cd7cbb34bcf70
arthasasta <116079643+arthasasta@users.noreply.github.com>
2023-04-17 20:33:55 +0700

    Update forminputmahasiswa.tsx

--- pages/forminputmahasiswa.tsx
+++ pages/forminputmahasiswa.tsx
@@ -1,50 +1,49 @@
-import { useState } from "react";
-import axios from "axios";
-import React from "react";
+import axios from 'axios';
+import { useState,useEffect } from "react";
 
 const client = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com/posts"
 });
 
-const [post, setPost] = useState(null);
-useEffect(() => {
-  async function getPost() {
-    const response = await client.get("/1");
-    setPost(response.data);
-  }
-  getPost();
-}, []);
-
-
-if (!post) return "No post!"
-async function deletePost() {
-await client.delete("/1");
-alert("Post deleted!");
-setPost(null);
-}
+const koneksiMahasiswa = axios.create({
+  // baseURL: "http://192.168.1.33:5000/api/mahasiswa"
+   baseURL: "http://127.0.0.1:5000/api/mahasiswa"
+ }); 
 
 export default function FormMahasiswa() {
     const [nama, setNama] = useState("");
     const [nim, setNim] = useState("");
-    const [post, setPost] = React.useState(null);
+    const [post, setPost] = useState(null);
+    const [mahasiswa, setMahasiswa] =  useState(null);
 
-    React.useEffect(() => {
+    useEffect(() => {
+      async function getMahasiswa() {
+        const response = await koneksiMahasiswa.get("/").then(function (axiosResponse) {
+            setMahasiswa(axiosResponse.data.data);
+         })
+         .catch(function (error) {
+         
+          alert('error from mahasiswa in api mahasiswa: '+error);
+         });;
+          }
+      getMahasiswa();
+    }, []);
+
+   
+    useEffect(() => {
       async function getPost() {
         const response = await client.get("/1");
         setPost(response.data);
       }
       getPost();
     }, []);
-    
-    
-    if (!post) return "No post!"
-    async function deletePost() {
+   
+  if (!post) return "No post!"
+  async function deletePost() {
     await client.delete("/1");
     alert("Post deleted!");
     setPost(null);
-    }
-
-
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -53,45 +52,81 @@ export default function FormMahasiswa() {
 
     return (
 
-
+<center><br></br><h1>Form Input Mahasiswa</h1><br></br>
 <div>
   <form onSubmit={handleSubmit}>
 
       <table border={0}>
         <tbody>
         <tr>
-        <td> <label> nim:</label></td>
+        <td> <label> Nim:</label></td>
         <td><input type="text" id="nim"  value={nim}
           onChange={(e) => setNim(e.target.value)}  /></td>
     </tr>
     <tr>
-        <td>  <label> nama:</label></td>
+        <td>  <label> Nama:</label></td>
         <td><input type="text" id="nama"  value={nama}
           onChange={(e) => setNama(e.target.value)} /></td>
     </tr>
        
     <tr>
-        <td>  <label> alamat:</label></td>
+        <td>  <label> Tanggal Lahir:</label></td>
+        <td>   <input
+                type="date" value="2018-07-22" min="1945-01-01" max="2024-12-31
+                "/></td>
+    </tr>
+
+    <tr>
+        <td>  <label> Agama:</label></td>
+        <td>   <select>
+            <option value="Pilih Agama">Pilih Agama</option>
+            <option value="Islam">Islam</option>
+            <option value="Kristen Protestan">Kristen Protestan</option>
+            <option value="Katolik">Katolik</option>
+            <option value="Hindu">Hindu</option>
+            <option value="Buddha">Buddha</option>
+            <option value="Konghucu">Konghucu</option>
+        </select></td>
+    </tr>
+
+    <tr>
+        <td>  <label> Alamat:</label></td>
         <td><textarea  id="address" style={{resize: "none"}} /></td>
     </tr>
-       
+    
     <tr>
-        <td>  <label> hobby:</label></td>
+        <td>  <label> Jurusan:</label></td>
         <td>   <select>
+            <option value="Pilih Jurusan">Pilih Jurusan</option>
+            <option value="Sistem Informasi">Sistem Informasi</option>
+            <option value="Teknik Informatika">Teknik Informatika</option>
+            <option value="Manajemen Informatika">Manajemen Informatika</option>
+            <option value="Akuntansi">Akuntansi</option>
+            <option value="Manajemen">Manajemen</option>
+        </select></td>
+    </tr>
+
+    <tr>
+        <td>  <label> Hobby:</label></td>
+        <td>   <select>
+            <option value="Pilih Hobby">Pilih Hobby</option>
             <option value="Musik">Musik</option>
             <option value="Olahraga">Olahraga</option>
             <option value="Membaca">Membaca</option>
+            <option value="Main Game">Main Game</option>
+            <option value="Nonton Film">Nonton Film</option>
         </select></td>
     </tr>
     <tr>
-        <td>  <label> gender:</label></td>
+        <td>  <label> Jenis Kelamin:</label></td>
         <td>   <input
-                type="radio" value="Pria" name="gender"/> Pria
+                type="radio" value="Pria" name="Jenis Kelamin"/> Pria
             <input
-                type="radio" value="Wanita"  name="gender"/> Wanita
-            <input
-                type="radio" value="Hermaprodit"  name="gender"/> Hermaprodit</td>
+            
+                type="radio" value="Wanita"  name="Jenis Kelamin"/> Wanita</td>
     </tr>
+    
+
     <tr>
         <td>  <label> Foto:</label></td>
         <td>   <input
@@ -99,8 +134,8 @@ export default function FormMahasiswa() {
     </tr>
         </tbody>
       </table>
-      <input type="submit" />
-      <audio   src="./audio/hbd.mp3" controls autoPlay/>
+
+      <br></br><input type="submit" /><br></br><br></br>
      
       </form>
       <table border={1}>
@@ -115,14 +150,13 @@ export default function FormMahasiswa() {
             </tr>
         </tbody>
       </table>
+      
       <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
+      {/* <h1>{post.title}</h1>
+      <p>{post.body}</p> */}
       <button onClick={deletePost}>Delete Post</button>
       </div>
-      </div>
-      
-    )
-  }
-   
 
+      <br/>
+      <br/>
+      <br/>
